df65f401e4fd98f7611e884145e0adae4da49c19
luckymenslife <ilgiz.sadykov@gmail.com>
2015-12-04 21:55:10 +0300

    fixed getToken, added doGet

--- main.go
+++ main.go
@@ -17,12 +17,13 @@ type AutomapInstance struct{
 	at AutomapToken
 }
 type AutobaseOrg struct{
-	Gid int64
+	Gid int
 	Name string
 	Address string
 	Stamp string
 	Phone string
 	Sync bool
+	External_id int
 }
 
 type AutomapConf struct {
@@ -69,6 +70,7 @@ func main(){
 		checkErr(err)
 		fmt.Printf("%d",id, objectType, changeType, changeDate, data)
 		var abOrg AutobaseOrg
+		abOrg.External_id = -1
 		err := json.Unmarshal([]byte(data),&abOrg)
 		checkErr(err)
 		fmt.Println(data)
@@ -78,6 +80,7 @@ func main(){
 		fmt.Println(abOrg.Stamp)
 		fmt.Println(abOrg.Phone)
 		fmt.Println(abOrg.Sync)
+		fmt.Println(abOrg.External_id)
 	}
 
 	automapInst := AutomapInstance{
@@ -95,13 +98,25 @@ func checkErr(err error){
 		panic(err)
 	}
 }
-
-func (ai AutomapInstance) doPost(req string,reqObj interface{},respObj interface{},useToken bool) (int,error){
+/*
+Выполнения POST-запроса. req - URI запроса, reqObj - структура (объект), передаваемая серверу при запросе.
+respObj - структура ответа сервера. useToken - флаг, указывающий, прикреплять ли к URI параметр с токеном.
+reqObj трансформируется в JSON и отсылается на веб.
+Ответ парсится из JSONа и запихивается в respObj
+ */
+func (ai AutomapInstance) doPost(req string,reqObj interface{},respObj interface{},useToken bool,params string) (int,error){
 	reqObjJson, err := json.Marshal(reqObj)
 	checkErr(err)
 	fullURL := ai.ac.url+req
 	if useToken{
 		fullURL += "?token="+ai.at.Token
+		if len(params)>0 {
+			fullURL += "&"+params
+		}
+	}else {
+		if len(params)>0 {
+			fullURL += "?"+params
+		}
 	}
 	//request, err := http.NewRequest("POST", fullURL, bytes.NewBuffer(reqObjJson))
 	//request.Header.Set("Content-Type", "application/json")
@@ -124,11 +139,54 @@ func (ai AutomapInstance) doPost(req string,reqObj interface{},respObj interface
 	}
 }
 
+/*
+Выполнения GET-запроса. req - URI запроса, respObj - структура ответа сервера.
+useToken - флаг, указывающий, прикреплять ли к URI параметр с токеном.
+params - строка параметров для запроса (Ex. firstname=Ilgiz&lastname=Sadykov)
+Ответ парсится из JSONа и запихивается в respObj
+ */
+func (ai AutomapInstance) doGet(req string,params string,respObj interface{},useToken bool) (int,error){
+	fullURL := ai.ac.url+req
+	if useToken{
+		fullURL += "?token="+ai.at.Token
+		if len(params)>0 {
+			fullURL += "&"+params
+		}
+	}else {
+		if len(params)>0 {
+			fullURL += "?"+params
+		}
+	}
+	client := &http.Client{}
+	response, err := client.Get(fullURL)
+	checkErr(err)
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return 0,err
+	}
+	if response.StatusCode != 200 {
+		return response.StatusCode,errors.New(string(body))
+	}else {
+		err = json.Unmarshal(body,respObj)
+		checkErr(err)
+		return 200,nil
+	}
+}
+
+
+/*
+Возвращение токена аутентификации.
+Если указан флаг force, то будет заново произведена аутентификация с помощью
+логина и пароля.
+В случае, если токен пустой, то это аналогично использованию флага force.
+Если срок действия токена вышел, то происходит процедура обновления токена.
+ */
 func (ai AutomapInstance) getToken(force bool) (string,error){
 	if force {
 		aAuth := AutomapAuth{ai.ac.login,ai.ac.password}
 		var respToken AutomapToken
-		statusCode,err := ai.doPost("/token/auth",aAuth,&respToken,false)
+		statusCode,err := ai.doPost("/token/auth",aAuth,&respToken,false,"")
 		fmt.Println(statusCode)
 		checkErr(err)
 		if statusCode == 200 {
@@ -142,6 +200,19 @@ func (ai AutomapInstance) getToken(force bool) (string,error){
 		curTime := time.Now()
 		ttl, _ := strconv.Atoi(ai.at.Ttl)
 		if curTime.Before(ai.at.updateTime.Add(time.Duration(ttl) * time.Second))||len(ai.at.Token)<2 {
+			if len(ai.at.Token)>1 {
+				var respToken AutomapToken
+				statusCode, err := ai.doGet("/token/refresh", "refreshToken=" + ai.at.RefreshToken,&respToken, false)
+				if statusCode == 200 {
+					ai.at = respToken
+					ai.at.updateTime = time.Now()
+					fmt.Println("Token refreshed.")
+					return respToken.Token, nil
+				}
+				if statusCode != 404 {
+					return "", err
+				}
+			}
 			return ai.getToken(true)
 		}else {
 			return ai.at.Token, nil
