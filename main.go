dfc2d8fde7f2dfdac3f1f63855e90a413f507dd9
luckymenslife <ilgiz.sadykov@gmail.com>
2015-12-08 18:16:44 +0300

    it s somehow working

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ import (
 )
 type AutomapInstance struct{
 	ac AutomapConf
-	at AutomapToken
+	at *AutomapToken
 }
 
 type DatabaseInstance struct{
 	dbconn string
+	db *sql.DB
 }
 
 type AutobaseOrg struct{
@@ -48,9 +49,25 @@ type AutomapToken struct{
 	Ttl string
 	updateTime time.Time
 }
+
+type AutomapOrginazation struct {
+	Id int
+	Address string
+	Email string
+	Name string
+	Phone string
+}
+
 var dbInst DatabaseInstance
+var automapInst AutomapInstance
 var db *sql.DB
 func main(){
+
+	automapInst = AutomapInstance{
+		AutomapConf{"http://mt.asuds77.ru/","ilgiz","anakonda"},
+		&AutomapToken{"","","",time.Now()}}
+
+
 	dbhost := "gbu.asuds77.ru"
 	dbport := "5432"
 	dbname := "inf_asuds_copy"
@@ -58,7 +75,7 @@ func main(){
 	dbpass := "ctrhtnysq!rjl"
 	dbconn := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",dbhost,dbport,dbname,dbuser,dbpass)
 
-	dbInst = DatabaseInstance{dbconn}
+	dbInst = DatabaseInstance{dbconn: dbconn,db: &sql.DB{}}
 	var err error
 	db,err = dbInst.connect()
 	fmt.Println(dbInst)
@@ -121,7 +138,7 @@ func checkErr(err error){
 
 func (di DatabaseInstance) connect() (*sql.DB,error) {
 	var err error
-	db,err = sql.Open("postgres",di.dbconn)
+	di.db,err = sql.Open("postgres",di.dbconn)
 	checkErr(err)
 	return db,err
 }
@@ -138,7 +155,7 @@ func (di DatabaseInstance) close(db *sql.DB) error {
 последовательности public.changes_id_seq
  */
 func (di DatabaseInstance) getSeqVal(db *sql.DB) (int, error) {
-	rows, err := db.Query("SELECT last_value+1 from public.changes_id_seq")
+	rows, err := di.db.Query("SELECT last_value from public.changes_id_seq")
 	checkErr(err)
 	var id int = -1
 	for rows.Next() {
@@ -148,7 +165,7 @@ func (di DatabaseInstance) getSeqVal(db *sql.DB) (int, error) {
 	return id,err
 }
 func (di DatabaseInstance) getLastId(db *sql.DB) (int, error) {
-	rows, err := db.Query("SELECT min(id) from public.changes where status = 0")
+	rows, err := di.db.Query("SELECT min(id)-1 from public.changes where status = 0")
 	checkErr(err)
 	var id int = -1
 	for rows.Next() {
@@ -162,7 +179,7 @@ func (di DatabaseInstance) getLastId(db *sql.DB) (int, error) {
 }
 
 func (di DatabaseInstance) processTable(db *sql.DB,lastID *int) (error) {
-	rows, err := db.Query("SELECT id, object_type, change_type, data from public.changes where status = 0 and id >= ? order by id limit 10",strconv.Itoa(*lastID))
+	rows, err := db.Query("SELECT id, object_type, change_type, data from public.changes where status = 0 and id>$1 order by id limit 10", *lastID)
 	checkErr(err)
 	for rows.Next() {
 		var id int
@@ -174,8 +191,69 @@ func (di DatabaseInstance) processTable(db *sql.DB,lastID *int) (error) {
 		checkErr(err)
 		*lastID = id
 		fmt.Println(data)
+		dbInst.processTask(db,id,objectType,changeType,data)
 	}
 	return err
+}
+
+func (di DatabaseInstance) processTask(db *sql.DB, id int, objectType string, changeType string, data string){
+	switch objectType{
+	case "ORG":
+		err := processOrg(changeType, data)
+		checkErr(err)
+	}
+	return
+}
+
+func processOrg(changeType string, data string) error{
+	var err error
+	switch {
+	case changeType == "INSERT" || changeType == "UPDATE":
+		var abOrg AutobaseOrg
+		abOrg.External_id = -1
+		err = json.Unmarshal([]byte(data),&abOrg)
+		//checkErr(err)
+		if abOrg.External_id > 0 {
+			err = updateOrg(abOrg)
+		}else{
+			var externalId int
+			externalId,err = createOrg(abOrg)
+			dbInst.updateExternalId("ORG",abOrg.Gid,externalId)
+		}
+		//checkErr(err)
+	}
+	return err
+}
+
+func updateOrg(abOrg AutobaseOrg) error{
+	ao_req:=AutomapOrginazation{abOrg.External_id,abOrg.Address,"",abOrg.Name, abOrg.Phone}
+	var ao_resp AutomapOrginazation
+	_,err := automapInst.doPut("/organizations/"+strconv.Itoa(abOrg.External_id),ao_req,&ao_resp,true,"")
+	return err
+}
+
+func createOrg(abOrg AutobaseOrg) (int,error){
+	ao_req:=AutomapOrginazation{-1,abOrg.Address,"",abOrg.Name, abOrg.Phone}
+	var ao_resp AutomapOrginazation
+	_,err := automapInst.doPost("/organizations",ao_req,&ao_resp,true,"")
+	if err ==nil{
+		return ao_resp.Id, err
+	}
+	return -1,err
+}
+
+func (di DatabaseInstance) updateExternalId(objectType string,gid int, externalId int){
+	var tableName string = ""
+	switch objectType{
+	case "ORG":
+		tableName = "autobase.orgs"
+	case "CAR":
+		tableName = "autobase.cars"
+	}
+	if len(tableName)>2 {
+		_, err := db.Exec("UPDATE " +tableName+" set external_id = $1 where gid = $2;",externalId,gid)
+		checkErr(err)
+	}
 }
 
 /*
@@ -189,7 +267,9 @@ func (ai AutomapInstance) doPost(req string,reqObj interface{},respObj interface
 	checkErr(err)
 	fullURL := ai.ac.url+req
 	if useToken{
-		fullURL += "?token="+ai.at.Token
+		token,err := ai.getToken(false)
+		checkErr(err)
+		fullURL += "?token="+token
 		if len(params)>0 {
 			fullURL += "&"+params
 		}
@@ -219,6 +299,43 @@ func (ai AutomapInstance) doPost(req string,reqObj interface{},respObj interface
 	}
 }
 
+func (ai AutomapInstance) doPut(req string,reqObj interface{},respObj interface{},useToken bool,params string) (int,error){
+	reqObjJson, err := json.Marshal(reqObj)
+	checkErr(err)
+	fullURL := ai.ac.url+req
+	if useToken{
+		token,err := ai.getToken(false)
+		checkErr(err)
+		fullURL += "?token="+token
+		if len(params)>0 {
+			fullURL += "&"+params
+		}
+	}else {
+		if len(params)>0 {
+			fullURL += "?"+params
+		}
+	}
+	request, err := http.NewRequest("PUT", fullURL, bytes.NewBuffer(reqObjJson))
+	request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	fmt.Println(fullURL)
+	fmt.Println(string(reqObjJson))
+	response, err := client.Do(request)
+	checkErr(err)
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return 0,err
+	}
+	if response.StatusCode != 200 {
+		return response.StatusCode,errors.New(string(body))
+	}else {
+		err = json.Unmarshal(body,respObj)
+		checkErr(err)
+		return 200,nil
+	}
+}
+
 /*
 Выполнения GET-запроса. req - URI запроса, respObj - структура ответа сервера.
 useToken - флаг, указывающий, прикреплять ли к URI параметр с токеном.
@@ -228,7 +345,9 @@ params - строка параметров для запроса (Ex. firstname=
 func (ai AutomapInstance) doGet(req string,params string,respObj interface{},useToken bool) (int,error){
 	fullURL := ai.ac.url+req
 	if useToken{
-		fullURL += "?token="+ai.at.Token
+		token,err := ai.getToken(false)
+		checkErr(err)
+		fullURL += "?token="+token
 		if len(params)>0 {
 			fullURL += "&"+params
 		}
@@ -238,6 +357,7 @@ func (ai AutomapInstance) doGet(req string,params string,respObj interface{},use
 		}
 	}
 	client := &http.Client{}
+	fmt.Println(fullURL)
 	response, err := client.Get(fullURL)
 	checkErr(err)
 	defer response.Body.Close()
@@ -270,21 +390,30 @@ func (ai AutomapInstance) getToken(force bool) (string,error){
 		fmt.Println(statusCode)
 		checkErr(err)
 		if statusCode == 200 {
-			ai.at = respToken
+			ai.at.RefreshToken = respToken.RefreshToken
+			ai.at.Token = respToken.Token
+			ai.at.Ttl = respToken.Ttl
 			ai.at.updateTime = time.Now()
+			fmt.Println(ai.at.RefreshToken)
+			fmt.Println(ai.at.Token)
+			fmt.Println(ai.at.Ttl)
+			fmt.Println(ai.at.updateTime)
 			fmt.Println("Authorized.")
 			return respToken.Token,nil
 		}
 		return "",err
 	}else {
+		fmt.Println(ai.at.Token)
 		curTime := time.Now()
 		ttl, _ := strconv.Atoi(ai.at.Ttl)
-		if curTime.Before(ai.at.updateTime.Add(time.Duration(ttl) * time.Second))||len(ai.at.Token)<2 {
+		if curTime.After(ai.at.updateTime.Add(time.Duration(ttl) * time.Second))||len(ai.at.Token)<2 {
 			if len(ai.at.Token)>1 {
 				var respToken AutomapToken
 				statusCode, err := ai.doGet("/token/refresh", "refreshToken=" + ai.at.RefreshToken,&respToken, false)
 				if statusCode == 200 {
-					ai.at = respToken
+					ai.at.RefreshToken = respToken.RefreshToken
+					ai.at.Token = respToken.Token
+					ai.at.Ttl = respToken.Ttl
 					ai.at.updateTime = time.Now()
 					fmt.Println("Token refreshed.")
 					return respToken.Token, nil
